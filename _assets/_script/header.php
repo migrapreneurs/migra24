7e232ec7c6c80b580ccab720ae0d36a020517d4a
vitobica <vito@migrapreneur.org>
2023-09-18 12:30:26 +0200

    JSON Home Update

--- _assets/_script/header.php
+++ _assets/_script/header.php
@@ -2,12 +2,12 @@
   <div class="logo"><img src="_assets/_images/migra-logo.svg" alt="Migra24 Logo" /></div>
   <nav>
     <ul>
-      <li><a class="btn standard bg-red text-white" href="#">Become an exhibitor</a></li>
+      <li><a class="btn standard bg-red text-white" href="/exhibitors">Become an exhibitor</a></li>
       <li><a href="#Why">Why attend</a></li>
       <li><a href="#Speakers">Speakers</a></li>
-      <li><a href="#">Partners</a></li>
-      <li><a href="#">FAQ</a></li>
-      <li><a class="btn standard bg-turquoise text-black" href="#">Buy Tickets</a></li>
+      <li><a href="#Partners">Partners</a></li>
+      <li><a href="#FAQ">FAQ</a></li>
+      <li><a class="btn standard bg-turquoise text-black" href="#Tickets">Buy Tickets</a></li>
     </ul>
   </nav>
   <div class="icon-container">
@@ -18,11 +18,11 @@
     <ul>
       <li><a href="#">Why attend</a></li>
       <li><a href="#Speakers">Speakers</a></li>
-      <li><a href="#">Partners</a></li>
-      <li><a href="#">FAQ</a></li>
+      <li><a href="#Partners">Partners</a></li>
+      <li><a href="#FAQ">FAQ</a></li>
       <div class="col-xs-12 mar-t-120">
-        <li><a class="btn large bg-turquoise text-black S" href="#">Buy Tickets</a></li>
-        <li><a class="btn large bg-red text-white S" href="#">Become an exhibitor</a></li>
+        <li><a class="btn large bg-turquoise text-black S" href="#Tickets">Buy Tickets</a></li>
+        <li><a class="btn large bg-red text-white S" href="/exhibitors">Become an exhibitor</a></li>
       </div>
     </ul>
   </div>
--- index.php
+++ index.php
@@ -42,7 +42,6 @@ $htmlMetaTags = generateMetaTags($metadata['metadata'][0]['meta']);
     // Decode the JSON data into an associative array
     $jsonData = json_decode($json_data, true);
 
-    var_dump($jsonData);
 ?>
 
 <!doctype html>
@@ -141,10 +140,10 @@ $htmlMetaTags = generateMetaTags($metadata['metadata'][0]['meta']);
           <section id="Why" class="row mar-t-120 col-xs-12">
             <h2 class="h1"><?php echo $jsonData['sections']['why']['title']; ?></h2>
             <p class="L col-lg-8"><?php echo $jsonData['sections']['why']['subtitle']; ?></p>
-            <ul class="highlight-box col-xs-12 mar-t-40">
+            <ul class="highlight-box col-xs-12 mar-t-60">
               <?php
               foreach ($jsonData['sections']['why']['benefits'] as $benefit) {
-                echo '<li class="">' . $benefit['text'] . '</li>';
+                echo '<li>' . $benefit['text'] . '</li>';
               }
               ?>
             </ul>
@@ -153,9 +152,9 @@ $htmlMetaTags = generateMetaTags($metadata['metadata'][0]['meta']);
 
 
           <!-- SPEAKERS -->
-          <section id="Speakers" class="mar-t-80">
-            <h6 class="mar-b-60">Draggable Card List</h6>
-            <div class="card-list-container">
+          <section id="Speakers">
+            <h2 class="h1">Our Speakers</h2>
+            <div class="mar-t-60 card-list-container">
               <div class="card-list">
                 <?php
             foreach ($jsonData['sections']['speakers'] as $speaker) {
@@ -169,6 +168,46 @@ $htmlMetaTags = generateMetaTags($metadata['metadata'][0]['meta']);
                 </div>
               <?php }} ?>
               </div>
+            </div>
+          </section>
+
+          <!-- PARTNERS -->
+          <section id="Partners">
+            <h2 class="h1">Our Speakers</h2>
+            <div class="mar-t-60 card-list-container">
+              <div class="card-list">
+                <?php
+            foreach ($jsonData['sections']['speakers'] as $speaker) {
+                if (!empty($speaker['fullname']) && !empty($speaker['jobtitle']) && !empty($speaker['url'])) { ?>
+                <div class="card">
+                  <img src="https://res.cloudinary.com/migrapreneur/image/upload/dpr_2.0,e_sharpen:139,q_auto:best/v1695030494/migra24/speakers/<?= $speaker['url']; ?>" alt="<?= $speaker['fullname']; ?>">
+                  <div class="card-text">
+                    <h3 class="L"><?= $speaker['fullname']; ?></h3>
+                    <p><?= $speaker['jobtitle']; ?></p>
+                  </div>
+                </div>
+              <?php }} ?>
+              </div>
+            </div>
+          </section>
+
+          <!-- FAQ -->
+          <section id="FAQ">
+
+            <div class="faq-container col-md-8 col-md-offset-2">
+              <h2 class="h1 mar-b-60">Frequently Asked Questions</h2>
+              <?php
+              foreach ($jsonData['sections']['faq'] as $faqItem) {
+                if (!empty($faqItem['question']) && !empty($faqItem['answer'])) { ?>
+              <div class="faq-item">
+                <input type="checkbox" class="faq-toggle" id="faq1">
+                <label for="faq1" class="faq-question">Question 1</label>
+                <div class="faq-answer">
+                  Answer 1 goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+              </div>
+            <?php }} ?>
+
             </div>
           </section>
 
